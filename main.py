c9c9b23f086e8ee60b1c4397567c941992dfc2c1
Test03 ACP <Test03pl@bbmag.bbraun.com>
2022-05-13 11:07:47 +0200

    I've created board

--- main.py
+++ main.py
@@ -1,29 +1,41 @@
+from re import X
 import pygame
 
+pygame.init()
 
-
+FPS = 60
 WIDTH, HEIGHT = 500, 500
 BLACK = (0, 0, 0)
-WHITE = (255, 255, 255)
+GREY = (96, 96, 96)
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Snake by b4nq")
 
 
 def draw_window():
     WIN.fill(BLACK)
-    for x in range(500):
-        pygame.draw.rect(WIN, WHITE, pygame.Rect(x, 0, 5, 500))
-    x += 50
+    x = 25
+    while x < 500:
+        pygame.draw.rect(WIN, GREY, pygame.Rect(x, 0, 1, 500))
+        x += 25
+    x = 25
+    while x < 500:
+        pygame.draw.rect(WIN, GREY, pygame.Rect(0, x, 500, 1))
+        x += 25
+    
     pygame.display.update()
 
 
 def main():
 
+    clock = pygame.time.Clock()
     run = True
     while run:
+        clock.tick(FPS)
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
+
+        draw_window()
 
     pygame.quit()
 
