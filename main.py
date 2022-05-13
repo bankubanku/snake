e11d1dd8db2439fdcc5e6627b58a699d47969013
b4nq <wozniczka.jakub04@gmail.com>
2022-05-13 13:00:33 +0200

    I've created function that creates board and snake.py file

--- main.py
+++ main.py
@@ -1,27 +1,23 @@
-from re import X
 import pygame
+import board
 
 pygame.init()
 
-FPS = 60
-WIDTH, HEIGHT = 500, 500
+# colors
 BLACK = (0, 0, 0)
 GREY = (96, 96, 96)
+RED = (255, 0, 0)
+# display settings 
+FPS = 60
+WIDTH, HEIGHT = 500, 500
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Snake by b4nq")
 
 
 def draw_window():
     WIN.fill(BLACK)
-    x = 25
-    while x < 500:
-        pygame.draw.rect(WIN, GREY, pygame.Rect(x, 0, 1, 500))
-        x += 25
-    x = 25
-    while x < 500:
-        pygame.draw.rect(WIN, GREY, pygame.Rect(0, x, 500, 1))
-        x += 25
-    
+    board.draw_board(WIN, GREY, WIDTH, HEIGHT, 20, 20)
+    pygame.draw.rect(WIN, RED, pygame.Rect(26, 26, 24, 24))
     pygame.display.update()
 
 
